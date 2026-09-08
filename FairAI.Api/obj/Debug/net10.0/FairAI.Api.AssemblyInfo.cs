6cfb067f09c9e2862a18bc11c4cca450836d4084
Artem <xoarty1002@gmail.com>
2026-09-08 15:59:35 +0000

    fixed gitignore by grok

--- FairAI.Api/obj/Debug/net10.0/FairAI.Api.AssemblyInfo.cs
+++ FairAI.Api/obj/Debug/net10.0/FairAI.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FairAI.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d72c3440f8e80c9cb9e976ad983ed726bcbf7f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c557dbfd201fde4e86c454221fd7925bc121227d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FairAI.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FairAI.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FairAI.Tests/obj/Debug/net10.0/FairAI.Tests.AssemblyInfo.cs
+++ FairAI.Tests/obj/Debug/net10.0/FairAI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FairAI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d72c3440f8e80c9cb9e976ad983ed726bcbf7f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c557dbfd201fde4e86c454221fd7925bc121227d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FairAI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FairAI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
