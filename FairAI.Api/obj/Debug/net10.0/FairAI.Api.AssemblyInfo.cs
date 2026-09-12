3b6503ff01c488244e22a5b7c80a9b895e443d21
Artem <xoarty1002@gmail.com>
2026-09-12 16:58:36 +0000

    fix log

--- FairAI.Api/obj/Debug/net10.0/FairAI.Api.AssemblyInfo.cs
+++ FairAI.Api/obj/Debug/net10.0/FairAI.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FairAI.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d346d44ba6a479f6b9b7e78c09b01bb106e00e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b36d5be5a27af90a2954e30917f2ba490a4ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FairAI.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FairAI.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
