04cce4d1a572c8a180afe3d0b40ed823b94e9313
Artem <xoarty1002@gmail.com>
2026-09-12 13:20:38 +0000

    db fix

--- FairAI.Tests/obj/Debug/net10.0/FairAI.Tests.AssemblyInfo.cs
+++ FairAI.Tests/obj/Debug/net10.0/FairAI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FairAI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cfb067f09c9e2862a18bc11c4cca450836d4084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc998f7a32b452725fcceed99770ee129d4c1480")]
 [assembly: System.Reflection.AssemblyProductAttribute("FairAI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FairAI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
